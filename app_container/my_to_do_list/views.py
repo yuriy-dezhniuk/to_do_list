a73c97afe8027c66ae2c99ccc66fd11712877ff2
dezhny2 <yurii.dezhniuk@medtronic.com>
2022-08-15 20:57:23 +0300

    create layout for separate task page

--- app_container/my_to_do_list/views.py
+++ app_container/my_to_do_list/views.py
@@ -10,4 +10,6 @@ def index(request):
 def task(request):
     return render(request, 'my_to_do_list/task.html', {
         'title': 'Title Task X',
+        'description': """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""",
+        'priority': 'P1',
     })
